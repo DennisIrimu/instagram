52acad027b01fd0bce2387374b4223cf52f6bcd9
dnyt <lyricaldnyt@gmail.com>
2018-06-19 16:56:01 +0300

    Created dashboard url

--- instagram/insta/urls.py
+++ instagram/insta/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     #url(r'^login/$', views.user_login, name='Login'),
     url(r'^login/$','django.contrib.auth.views.login',name='login'),
     url(r'^logout/$','django.contrib.auth.views.logout',name='logout'),
-    url(r'^logout-then-login/$','django.contrib.auth.views.logout_then_login',name='logout_then_login')
+    url(r'^logout-then-login/$','django.contrib.auth.views.logout_then_login',name='logout_then_login'),
+    url(r'^$', views.dashboard, name='dashboard'),
 ]
 
 #if settings.DEBUG:
