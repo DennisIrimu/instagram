2e699a54e8e11792a79ea491cef4b3ca4ae65fd6
dnyt <lyricaldnyt@gmail.com>
2018-06-19 19:28:07 +0300

    Added reset password urls

--- instagram/insta/urls.py
+++ instagram/insta/urls.py
@@ -10,6 +10,10 @@ urlpatterns = [
     url(r'^$', views.dashboard, name='dashboard'),
     url(r'^password-change/$',password_change,name='password_change'),
     url(r'^password-change/done/$',password_change_done,name='password_change_done'),
+    url(r'^password-reset/$'password_reset,name='password_reset'),
+    url(r'^password-reset/done/$'password_reset_done,name='password_reset_done'),
+    url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$'password_reset_confirm,name=password_reset_confirm),
+    url(r'^password_reset/complete/$'password_reset_complete,name='password_reset_complete'),
 ]
 
 #if settings.DEBUG:
