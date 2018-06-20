7ef55b5da281d7c8611c137bec28835e2e1f344b
dnyt <lyricaldnyt@gmail.com>
2018-06-20 10:38:57 +0300

    Create model Profile class

--- instagram/insta/models.py
+++ instagram/insta/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from djang.conf import settings
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    photo = models.ImageField(upload_to='users',blank=True)
+
+def __str__(self):
+    return 'Profile for user {}'.format(self.user.username)
