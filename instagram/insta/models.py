b0af03c4d9bac537de0d2f98e7724d3390d4babc
dnyt <lyricaldnyt@gmail.com>
2018-06-20 22:38:51 +0300

    Create model Image post class

--- instagram/insta/models.py
+++ instagram/insta/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.conf import settings
+import datetime as dt
 # Create your models here.
 class Profile(models.Model):
     #user = models.OneToOneField(settings.AUTH_USER_MODEL)
     profile_photo = models.ImageField(upload_to='users',blank=True)
     bio = models.CharField(max_length=60)
-    profile_user = models.ForeignKey(User,on_delete=models.CASCADE)
+    #profile_user = models.ForeignKey(User,on_delete=models.CASCADE)
 
     def save_profile(self):
         self.save()
@@ -13,6 +14,24 @@ class Profile(models.Model):
     def delete_profile(self):
         self.delete()
 
-    
+
 def __str__(self):
     return 'Profile for user {}'.format(self.user.username)
+
+class Image(models.Model):
+
+    image=models.ImageField(upload_to = 'posts/',blank=True)
+    image_name=models.CharField(max_length=60)
+    image_caption=models.CharField(max_length=60)
+    image_profile=models.ForeignKey(Profile,null=True)
+    pub_date=models.DateTimeField(auto_now_add=True)
+    #likes=models.ManyToManyField(User,related_name="likes",default=None)
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def __str__(self):
+        return self.image_name
