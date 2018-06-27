5c8a436f053b596bdc34db29f088f9b7b85a4b49
dnyt <lyricaldnyt@gmail.com>
2018-06-27 09:47:05 +0300

    Created ImageTestClass

--- insta/tests.py
+++ insta/tests.py
@@ -27,3 +27,41 @@ class ProfileTestClass(TestCase):
 
     def tearDown(self):
         Image.objects.all().delete()
+
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.user = User(username='dnyt',email='lyricaldnyt@gmail.com',password='dimm8450')
+        self.user.save()
+        self.likes=likes(profile=self.user)
+        self.likes.save()
+        self.profile=Profile(profile_photo='/posts',bio='yadiyadi ya',profile_user=self.user)
+        self.image=Image
+        self.image.likes.add(self.user)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Image,Image))
+
+    def test_save_image(self):
+        self.image.save_image()
+        images= Image.objets.all()
+        self.assertTrue(len(images) > 0)
+
+    def test_update_caption(self):
+        self.image.save_image()
+        kwargs={'image':'/posts','image_name':'myname','image_caption':'myself'}
+        Image.update_caption(self.inage.id,**kwargs)
+        self.assertEqual("myname",self.image.image_name)
+
+    def test_delete_image(self):
+        self.image.save_image()
+        self.image.delete_image()
+        images = Imagge.objects.all()
+        self.assertTrue(len(locations) == 0)
+
+    def test_get_image_id(self):
+        image_id = id
+        self.image.objects.get(pk=id)
+        self.assertTrue(pk=id)
+
+    def tearDown(self):
+        Image.objects.all().delete()
